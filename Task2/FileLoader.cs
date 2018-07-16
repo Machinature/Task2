22a47697bedee89beb75d7934516153bf4dbee35
Vadim Ivanov <mrvadim2000@gmail.com>
2018-07-16 18:12:04 +0300

    Added a default data if the file is empty or doesn't exist

--- Task2/FileLoader.cs
+++ Task2/FileLoader.cs
@@ -9,17 +9,29 @@ namespace Task2
 
 		public FileLoader(string filename)
 		{
-			streamReader = new StreamReader(new FileStream(filename, FileMode.Open));
+			streamReader = new StreamReader(new FileStream(filename, FileMode.OpenOrCreate));
+			string checkString = streamReader.ReadLine();
+			if (checkString == null)
+				SetDefaultDataToFile();
 		}
 
 		public string[] LoadData()
 		{
-			string input = streamReader.ReadLine();
+			streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+			string input = streamReader.ReadToEnd();
 			_strings = input.Split(' ');
 
 			streamReader.Dispose();
 
 			return _strings;
+		}
+
+		private void SetDefaultDataToFile()
+		{
+			string defaultString = "abaa bbb ccbacd";
+			StreamWriter streamWriter = new StreamWriter(streamReader.BaseStream);
+			streamWriter.WriteLine(defaultString);
+			streamWriter.Flush();
 		}
 	}
 }
